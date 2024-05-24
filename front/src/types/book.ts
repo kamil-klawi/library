4c95cb91da5723aca9fd4f0d3fb928a07c8ffeb7
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2024-05-24 18:54:37 +0200

    chore(types): add types

---
+++ front/src/types/book.ts
@@ -0,0 +1,13 @@
+export interface Book {
+    id: number;
+    title: string;
+    publishingHouse: string;
+    languageOriginal: string;
+    numberOfPages: number;
+    publicationDate: Date;
+    type: string;
+    cover: string;
+    height: number;
+    width: number;
+    depth: number;
+}
