5acd991a8c537a97790f0cf85c0fd4ad3e4d94e6
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2024-05-27 17:42:03 +0200

    chore(app): add routing

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,11 +2,31 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import Homepage from './pages/Homepage';
 import Error from './pages/Error';
+import Books from './pages/Books';
+import Translators from './pages/Translators';
+import Authors from './pages/Authors';
+import About from './pages/About.tsx';
 
 const routerProvider = createBrowserRouter([
     {
         path: '/',
         element: <Homepage />,
+    },
+    {
+        path: '/about_project',
+        element: <About />,
+    },
+    {
+        path: '/books',
+        element: <Books />,
+    },
+    {
+        path: '/authors',
+        element: <Authors />,
+    },
+    {
+        path: '/translators',
+        element: <Translators />,
     },
     {
         path: '*',
