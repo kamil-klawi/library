43e25789ccb0ea60ec9090818eb4df95ed580fe3
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2024-05-27 17:41:48 +0200

    chore: add few tweaks

--- front/src/components/Card.tsx
+++ front/src/components/Card.tsx
@@ -3,10 +3,10 @@ import { InfoCardProps } from '../types/card.ts';
 import styles from '../styles/components/Card.module.scss';
 import { Link } from 'react-router-dom';
 
-export const Card: FC<InfoCardProps> = ({ title, description, image }) => {
+export const Card: FC<InfoCardProps> = ({ uri, title, description, image }) => {
     return (
         <div className={styles.card}>
-            <Link to={title}>
+            <Link to={uri}>
                 <h2 className={styles.card__title}>{title}</h2>
                 <div className={styles.card__content}>
                     <p className={styles.card__text}>{description}</p>
